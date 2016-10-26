4cc3e278d93358e0f26f13d5948ec628a99c4268
SWPUliusong <1418441382@qq.com>
2016-10-26 16:00:07 +0800

    update

--- promise/p.js
+++ promise/p.js
@@ -10,7 +10,6 @@
     }
   }
 
-
   function P(fn) {
     if (typeof fn !== 'function') {
       throw new Error('the arguments is not a function')
@@ -22,23 +21,71 @@
   }
   P.author = 'liusong'
   P.version = '0.0.1'
+
+  // 直接转交成功数据
+  P.resolve = function(data) {
+    return new P(function(resolve, reject) {
+      // 延时等待任务队列转交
+      setTimeout(function() {
+        resolve(data)
+      }, 0)
+    })
+  }
+
+  // 直接转交失败原因
+  P.reject = function(data) {
+    return new P(function(resolve, reject) {
+      // 延时等待任务队列转交
+      setTimeout(function() {
+        reject(data)
+      }, 0)
+    })
+  }
+
+  // 批处理
+  P.all = function(arr) {
+    return new P(function(resolve, reject) {
+      if (arr instanceof Array) {
+        reject('P.all need a Array')
+      }
+
+      let res = [],   // 存储结果
+        pending = 0;  // 记录状态
+
+      arr.forEach((p, i) => {
+        if (p.isPromise) {
+          pending++   // 注册异步任务时 +1
+          p.then((data) => {
+            pending--   // 完成异步任务时 -1
+            res[i] = data
+            if (pending === 0) {  // 为0时，全部完成
+              resolve(res)
+            }
+          })
+        } else {
+          // 不是Promise则直接当成数据
+          res[i] = p
+        }
+      })
+    })
+  }
   
+  // 成功态
   P.prototype.resolve = function(data) {
     //只触发接下来第一个控制器
     var handler = this.queue.shift()
     if (handler && handler.success) {
       let next = handler.success(data)
       if (next && next.isPromise) {
-        let self = this
         //任务队列转交
-        next.queue = self.queue
-        next.errorHandler = self.errorHandler
-        self = next
+        next.queue = this.queue
+        next.errorHandler = this.errorHandler
         return 
       }
     }
   }
 
+  // 失败态
   P.prototype.reject = function(error) {
     //如果catch监听则放弃执行then方法的第二个参数
     if (this.errorHandler) {
@@ -51,9 +98,7 @@
     if (handler && handler.error) {
       let next = handler.error(data)
       if (next && next.isPromise) {
-        let self = this
-        next.queue = self.queue
-        self = next
+        next.queue = this.queue
         return 
       }
     }
@@ -61,7 +106,7 @@
 
   P.prototype.then = function(resolve, reject) {
     if (typeof resolve !== 'function') {
-      throw new Error('the arguments is not a function')
+      throw new Error('the resolve is not a function')
     }
     var handler = {}
 
@@ -78,7 +123,7 @@
 
   P.prototype.catch = function(reject) {
     if (typeof reject !== 'function') {
-      throw new Error('the arguments is not a function')
+      throw new Error('the reject is not a function')
     }
     this.errorHandler = reject
     return
--- promise/test.js
+++ promise/test.js
@@ -1,41 +1,33 @@
+'use strict'
+
 const P = require("./p")
+const assert = require("assert")
+
+let deep = assert.deepEqual.bind(assert)
 
 function test1() {
   return new P(function(resolve, reject) {
     setTimeout(function() {
-      resolve(123)
+      resolve(100)
     }, 1000)
   })
 }
 
-function test2() {
-  return new P(function(resolve, reject) {
-    setTimeout(function() {
-      reject("404")
-    }, 300)
-  })
-}
-
-test1().then(function(data) {
-  console.log("1: " + data)
+P.all([test1(), 236, test1()])
+.then(function(data) {
+  deep(data, [100, 236, 100], data + '!= [100, 236, 100]')
   return test1()
 })
 .then(function(data) {
-  console.log("1: " + (data + 345))
-})
-.catch(function(err) {
-  console.log("1: " + err)
-})
-
-test1().then(function(data) {
-  console.log("2: " + data)
-  return test2()
+  deep(data, 100, data + '!= 100')
+  return P.resolve(200)
 })
 .then(function(data) {
-  console.log("2: " + (data + 345))
+  deep(data, 200, data + '!= 200')
+  return P.reject(500)
 })
 .catch(function(err) {
-  console.log("2: " + err)
+  deep(err, 500, err + '!= 500')
 })
 
 console.log(P.author)
