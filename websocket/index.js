12f36375455ccd7cfb42ef1cb00462f995ad77f9
SWPUliusong <swpuliusong@foxmail.com>
2017-07-20 23:59:35 +0800

    握手

--- websocket/index.js
+++ websocket/index.js
@@ -1,13 +1,14 @@
 const net = require("net")
 const crypto = require("crypto")
 
+// 加密Sec-WebSocket-Key的固定字符串
 let mask = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
 
 net.createServer(socket => {
 	let key
-	socket.on('data', res => {
+	socket.on('data', data => {
 		if (!key) {
-			key = res.toString().match(/Sec-WebSocket-Key: (.+)/)[1]
+			key = data.toString().match(/Sec-WebSocket-Key: (.+)/)[1]
 			key = crypto.createHash('sha1').update(key + mask).digest('base64')
 
 			socket.write("HTTP/1.1 101 Switching Protocals\r\n")
@@ -17,6 +18,13 @@ net.createServer(socket => {
 			socket.write("\r\n")
 		} else {
 			// 数据处理
+			onmessage(data)
 		}
 	})
-}).listen(5000)
+}).listen(5000, () => {
+	console.log('listening at 127.0.0.1:5000')
+})
+
+function onmessage(data) {
+	console.log(data)
+}
