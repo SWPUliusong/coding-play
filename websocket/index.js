1ba64625e141b048a1d75e2cb1d4f78cd7324203
liusong <swpuliusong@foxmail.com>
2017-07-21 17:33:23 +0800

    编码数据

--- websocket/index.js
+++ websocket/index.js
@@ -2,11 +2,12 @@ const net = require("net")
 const crypto = require("crypto")
 
 let decodeData = require("./decodeData")
+let encodeData = require("./encodeData")
 
 // 加密Sec-WebSocket-Key的固定字符串
 let mask = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
 
-net.createServer(socket => {
+let server = net.createServer(socket => {
 	let key
 	socket.on('data', data => {
 		if (!key) {
@@ -18,16 +19,37 @@ net.createServer(socket => {
 			socket.write("Connection: Upgrade\r\n")
 			socket.write(`Sec-WebSocket-Accept: ${key}\r\n`)
 			socket.write("\r\n")
+
+			// 握手成功发送数据
+			socket.write(encodeData({
+				FIN: 1,
+				Opcode: 1,
+				PayloadData: 'hello, 世界'
+			}))
+
+			// 分片传输数据
+			socket.write(encodeData({
+				FIN: 0, Opcode: 1, PayloadData: "ABC"
+			}));
+			socket.write(encodeData({
+				FIN: 0, Opcode: 0, PayloadData: "-DEF-"
+			}));
+			setTimeout(() => {
+				socket.write(encodeData({
+					FIN: 1, Opcode: 0, PayloadData: "GHI"
+				}));
+			}, 500)
 		} else {
 			// 数据处理
-			onmessage(data)
+			onmessage(socket, data)
 		}
 	})
 }).listen(5000, () => {
 	console.log('listening at 127.0.0.1:5000')
 })
 
-function onmessage(data) {
+function onmessage(socket, data) {
 	data = decodeData(data)
+	console.log(`${socket.remoteAddress} 发来消息：`)
 	console.log(data)
 }
